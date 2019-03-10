949086d728f64d798fff4229dcf483af2db08baf
ValeraBahleiUA <vbaglej@gmail.com>
2019-03-10 15:26:07 +0200

    AddedTabs

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,9 @@
 $(function(){
+    $(".tab_item").not(":first").hide();
+    $(".tabs-wrapper .tab").click(function() {
+        $(".tabs-wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+        $(".tab_item").hide().eq($(this).index()).fadeIn()
+    }).eq(0).addClass("active");
 
     var mixer = mixitup('.products__container');
     
